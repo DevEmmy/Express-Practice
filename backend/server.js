922579deecf7babc10d3bc78bb867f550e32e1e2
DevEmmy <eolaosebikan60@gmail.com>
2022-04-16 05:13:24 +0100

    Installed Mongoose and configured its connection to my Databse

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 const router = require('./routes/router')
+const mongoose = require('mongoose')
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+const uri = process.env.DATABASE_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection
+connection.once('open', ()=>{console.log('Database running Successfully')})
 
 
 const port = process.env.PORT || 5000
 app.use('/api/goals', router)
+
 
 
 app.listen(port, ()=>{
